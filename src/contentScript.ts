28f7b842a8fabc9906723e9d85cf30c983299d52
takagi <iam.takagi.dev@gmail.com>
2022-05-06 04:28:37 +0900

    Feat: ローカルの動画ファイルを参照、再生するように

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -2,20 +2,26 @@ async function play() {
   const body = document.body || document.getElementsByTagName("body")[0];
 
   if (body !== null) {
-    const video = document.createElement("video") as HTMLVideoElement
+    const video = document.createElement("video") as HTMLVideoElement;
     const target = body.querySelector("#sc-active-cart,#bag-content");
     if (target !== null) {
-        video.src = "https://ytwp.iamtakagi.net/stream?url=https://www.youtube.com/watch?v=X8v20C2X2Ag";
-        video.autoplay = true;
-        video.controls = true;
-        video.style.maxWidth = '100%';
-        target.appendChild(video);
-
-        setTimeout(async () => {
-          await video.play();
-        }, 1000);
+      const blob = await fetch(chrome.runtime.getURL('video.mp4')).then((r) => r.blob());
+      function createObjectURL(obj: Blob | MediaSource) {
+        return window.URL
+          ? window.URL.createObjectURL(obj)
+          : window.webkitURL.createObjectURL(obj);
+      }
+      const url = createObjectURL(blob);
+      video.src = url
+      video.autoplay = true;
+      video.controls = true;
+      video.style.maxWidth = '100%';
+      target.appendChild(video);
+      setTimeout(() => {
+        video.play()
+      }, 500);
     }
   }
 }
 
-play();
+play()
